bb618fbdf0d41bd7ba44db62ec8e2c344939b14f
Virgil <virgil@nosoft.io>
2022-11-20 11:24:14 +0100

    Minor behavior change

--- Scripts/Player/PlayerControl.cs
+++ Scripts/Player/PlayerControl.cs
@@ -7,6 +7,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
+    [SerializeField] private PlayerDying dyingScript;
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float speed;
@@ -19,7 +20,7 @@ public class PlayerControl : MonoBehaviour
     private float time;
     //Changement d'état vol / sol    
     public string state = "cube";
-    public void ChangeState(string value) { state = value; jumpSpeed = 0; }
+    public void ChangeState(string value) { state = value; jumpSpeed = 0;ResetRotation(); }
 
 
     private void FixedUpdate()
@@ -32,6 +33,7 @@ public class PlayerControl : MonoBehaviour
         rb.velocity = new Vector2(speed * time, jumpSpeed * time * (state=="ship" ? 1.1f : 1) );
     }
     private void cubeMovement() {
+        if(IsRoofed()) StartCoroutine(dyingScript.Die());
         //Si je suis au sol
         if (IsGrounded()) {
             //Ternaire : Si je veux sauter en boucle (input pas lacher) je re saute sinon ma valeur de saut passe a 0
@@ -61,20 +63,39 @@ public class PlayerControl : MonoBehaviour
         //Sinon je chute
         else if (!isJumping)
             jumpSpeed -= fallSpeed * 0.75f * time;
-        //Sinon je m'envole doucement
-        else jumpSpeed += fallSpeed * 0.75f * time;
-        if (!IsGrounded()) {
-            if (isJumping)
-                rb.MoveRotation(rb.rotation * time);
-            else rb.MoveRotation(rb.rotation * -time);
+        //Sinon je m'envole doucement si je ne suis pas au plafond
+        else if (!IsRoofed()) jumpSpeed += fallSpeed * 0.75f * time;
+        //Si je suis au plafond je met ma vitesse de saut a 0 pour pas être ralenti par celui-ci
+        else jumpSpeed = 0;
+        if (!IsGrounded() && !IsRoofed()) {
+            if (isJumping) {
+                Vector3 movement = new Vector3(speed, 0, jumpSpeed).normalized;
+                Quaternion targetRotation = Quaternion.LookRotation(movement);
+                targetRotation = Quaternion.RotateTowards(
+                                    transform.rotation,
+                                     targetRotation,
+                                        360 * Time.fixedDeltaTime);
+                Debug.Log(targetRotation);
+                rb.MoveRotation(targetRotation);
+            }
+            else {
+                Vector3 movement = new Vector3(speed, 0, jumpSpeed).normalized;
+                Quaternion targetRotation = Quaternion.LookRotation(movement);
+                targetRotation = Quaternion.RotateTowards(
+                                    transform.rotation,
+                                     targetRotation,
+                                        360 * Time.fixedDeltaTime);
+                Debug.Log(targetRotation);
+                rb.MoveRotation(targetRotation);
+            };
         }
         //au sol j'arrondi pour que sa prite soit droite
-        else {
-            Vector3 rotation = gameObject.transform.rotation.eulerAngles;
-            rotation.z = Mathf.Round(rotation.z / 90) * 90;
-            gameObject.transform.rotation = Quaternion.Euler(rotation);
-        }
-
+        else ResetRotation();
+    }
+    private void ResetRotation() {
+        Vector3 rotation = gameObject.transform.rotation.eulerAngles;
+        rotation.z = Mathf.Round(rotation.z / 90) * 90;
+        gameObject.transform.rotation = Quaternion.Euler(rotation);
     }
     //Bind sur les touches pour "sauter"
     public void OnJump(InputAction.CallbackContext context)
@@ -101,5 +122,9 @@ public class PlayerControl : MonoBehaviour
     private bool IsGrounded()
     {
         return Physics2D.Raycast(transform.position, Vector2.down, distanceFromGround + 0.05f,groundMask);
+    }
+    //Je raycast du centre de mon cube vers le plafond pour savoir si il est assez proche du plafond pour dire qu'il est "roofed"
+    private bool IsRoofed() {
+        return Physics2D.Raycast(transform.position, Vector2.up, distanceFromGround + 0.05f, groundMask);
     }
 }
